4234738c139927439daca768515e894b7bcaca01
Benjamin Kovach <bkovach5@uga.edu>
2015-02-06 15:51:53 -0500

    Fix type sigs

--- src/Audio/WebAudio/AnalyserNode.purs
+++ src/Audio/WebAudio/AnalyserNode.purs
@@ -6,41 +6,41 @@ import Data.Foreign.OOFFI
 
 {- Getters/Setters for all AnalyserNode properties -}
 
-setFftSize :: forall wau eff. OscillatorNode -> Number -> Eff (wau :: WebAudio | eff) Number
+setFftSize :: forall wau eff. AnalyserNode -> Number -> Eff (wau :: WebAudio | eff) Number
 setFftSize = setter "fftSize"
 
-getFftSize :: forall wau eff. OscillatorNode -> Eff (wau :: WebAudio | eff) Number
+getFftSize :: forall wau eff. AnalyserNode -> Eff (wau :: WebAudio | eff) Number
 getFftSize = getter "fftSize"
 
-getFrequencyBinCount :: forall wau eff. OscillatorNode -> Eff (wau :: WebAudio | eff) Number
+getFrequencyBinCount :: forall wau eff. AnalyserNode -> Eff (wau :: WebAudio | eff) Number
 getFrequencyBinCount = getter "frequencyBinCount"
 
-setMaxDecibels :: forall wau eff. OscillatorNode -> Number -> Eff (wau :: WebAudio | eff) Number
+setMaxDecibels :: forall wau eff. AnalyserNode -> Number -> Eff (wau :: WebAudio | eff) Number
 setMaxDecibels = setter "maxDecibels"
 
-getMaxDecibels :: forall wau eff. OscillatorNode -> Eff (wau :: WebAudio | eff) Number
+getMaxDecibels :: forall wau eff. AnalyserNode -> Eff (wau :: WebAudio | eff) Number
 getMaxDecibels = getter "maxDecibels"
 
-setMinDecibels :: forall wau eff. OscillatorNode -> Number -> Eff (wau :: WebAudio | eff) Number
+setMinDecibels :: forall wau eff. AnalyserNode -> Number -> Eff (wau :: WebAudio | eff) Number
 setMinDecibels = setter "MinDecibels"
 
-getMinDecibels :: forall wau eff. OscillatorNode -> Eff (wau :: WebAudio | eff) Number
+getMinDecibels :: forall wau eff. AnalyserNode -> Eff (wau :: WebAudio | eff) Number
 getMinDecibels = getter "minDecibels"
 
-setSmoothingTimeConstant :: forall wau eff. OscillatorNode -> Number -> Eff (wau :: WebAudio | eff) Number
+setSmoothingTimeConstant :: forall wau eff. AnalyserNode -> Number -> Eff (wau :: WebAudio | eff) Number
 setSmoothingTimeConstant = setter "smoothingTimeConstant"
 
-getSmoothingTimeConstant :: forall wau eff. OscillatorNode -> Eff (wau :: WebAudio | eff) Number
+getSmoothingTimeConstant :: forall wau eff. AnalyserNode -> Eff (wau :: WebAudio | eff) Number
 getSmoothingTimeConstant = getter "smoothingTimeConstant"
 
-getByteFrequencyData :: forall eff. OscillatorNode -> Eff (wau :: WebAudio | eff) [Number]
+getByteFrequencyData :: forall eff. AnalyserNode -> Eff (wau :: WebAudio | eff) [Number]
 getByteFrequencyData = method0Eff "getByteFrequencyData"
 
-getByteTimeDomainData :: forall eff. OscillatorNode -> Eff (wau :: WebAudio | eff) [Number]
+getByteTimeDomainData :: forall eff. AnalyserNode -> Eff (wau :: WebAudio | eff) [Number]
 getByteTimeDomainData = method0Eff "getByteTimeDomainData"
 
-getFloatFrequencyData :: forall eff. OscillatorNode -> Eff (wau :: WebAudio | eff) [Number]
+getFloatFrequencyData :: forall eff. AnalyserNode -> Eff (wau :: WebAudio | eff) [Number]
 getFloatFrequencyData = method0Eff "getFloatFrequencyData"
 
-getFloatTimeDomainData :: forall eff. OscillatorNode -> Eff (wau :: WebAudio | eff) [Number]
+getFloatTimeDomainData :: forall eff. AnalyserNode -> Eff (wau :: WebAudio | eff) [Number]
 getFloatTimeDomainData = method0Eff "getFloatTimeDomainData"
