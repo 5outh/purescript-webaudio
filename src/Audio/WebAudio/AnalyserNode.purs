65645d103435ad88c6f190dafc4035baea5f3a43
Benjamin Kovach <bkovach5@uga.edu>
2015-02-06 15:39:17 -0500

    Some stuff

---
+++ src/Audio/WebAudio/AnalyserNode.purs
@@ -0,0 +1,11 @@
+module Audio.WebAudio.AnalyserNode where
+
+import Control.Monad.Eff
+import Audio.WebAudio.Types
+import Data.Foreign.OOFFI
+
+setFftSize :: forall wau eff. OscillatorNode -> Number -> Eff (wau :: WebAudio | eff) Number
+setFftSize = setter "fftSize"
+
+getFftSize :: forall wau eff. OscillatorNode -> Number -> Eff (wau :: WebAudio | eff) Number
+getFftSize = getter "fftSize"
